fd091a574e389c6a5693d475e5c9f56adb747e5b
almightyk11 <takada.chopin.twitter@gmail.com>
2022-12-28 19:35:09 +0900

    chore: Mantineの導入と表示テスト

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,8 @@
-import { Head, Html, Main, NextScript } from 'next/document'
+import { createGetInitialProps } from '@mantine/next'
+import Document from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang='en'>
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+const getInitialProps = createGetInitialProps()
+
+export default class _Document extends Document {
+  static getInitialProps = getInitialProps
 }
