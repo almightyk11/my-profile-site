9889c339a46286c5f3a3248e9f33f67585348f51
almightyk11 <takada.chopin.twitter@gmail.com>
2022-12-29 23:11:58 +0900

    feat: カテゴリのラベルを増やす

--- components/Movie/MovieCard.tsx
+++ components/Movie/MovieCard.tsx
@@ -1,5 +1,6 @@
 import { Badge, Card } from '@mantine/core'
 import Movie from 'interfaces/movie'
+import { useMemo } from 'react'
 import ReactPlayer from 'react-player'
 
 type MovieCardProps = {
@@ -7,6 +8,11 @@ type MovieCardProps = {
 }
 
 export const MovieCard = (props: MovieCardProps) => {
+  const badgeColor = useMemo(() => {
+    if (props.movie.category === 'クラシック') return 'pink'
+    if (props.movie.category === 'アニメ') return 'green'
+    return 'blue'
+  }, [props.movie.category])
   return (
     <Card shadow='sm' radius='md' withBorder className='mb-5'>
       <Card.Section>
@@ -15,7 +21,7 @@ export const MovieCard = (props: MovieCardProps) => {
       <p className='mt-4 mb-0 font-semibold'>{props.movie.title}</p>
       <div className='flex justify-between mt-2 mb-1'>
         <p className='my-0 text-sm text-slate-600'>{props.movie.creator}</p>
-        <Badge color={props.movie.category === 'クラシック' ? 'pink' : 'green'} variant='light'>
+        <Badge color={badgeColor} variant='light'>
           {props.movie.category}
         </Badge>
       </div>
