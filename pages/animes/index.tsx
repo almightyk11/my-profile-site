71063b970e55c75fbc2f87c7e87dd8c2711c7a20
almightyk11 <takada.chopin.twitter@gmail.com>
2023-01-02 17:39:32 +0900

    fix: 表示崩れを修正

--- pages/animes/index.tsx
+++ pages/animes/index.tsx
@@ -1,4 +1,4 @@
-import { Accordion, Container, Rating } from '@mantine/core'
+import { Accordion, Container, Divider, Rating } from '@mantine/core'
 import { mockAnimeListByYear } from 'mock/animes'
 
 const Animes = () => {
@@ -17,18 +17,30 @@ const Animes = () => {
                 <p className='m-0 font-semibold text-[1rem]'>{animeList.year}</p>
               </Accordion.Control>
               <Accordion.Panel>
-                <ul className='text-slate-600'>
-                  {animeList.animes.map((anime) => (
-                    <li key={anime.title} className='flex justify-between'>
-                      <span>{anime.title}</span>
-                      {anime.rating ? <Rating count={3} value={anime.rating} /> : ''}
-                    </li>
+                <ul className='text-slate-600 '>
+                  {animeList.animes.map((anime, index) => (
+                    <>
+                      <li key={anime.title} className='flex justify-between my-1.5'>
+                        <span className='w-3/4'>{anime.title}</span>
+                        {anime.rating ? (
+                          <Rating count={3} value={anime.rating} className='1/4' />
+                        ) : (
+                          ''
+                        )}
+                      </li>
+                      {index === animeList.animes.length - 1 ? (
+                        ''
+                      ) : (
+                        <Divider my='sm' variant='dashed' />
+                      )}
+                    </>
                   ))}
                 </ul>
               </Accordion.Panel>
             </Accordion.Item>
           ))}
         </Accordion>
+        <p className='text-center text-slate-600'>これより前も随時更新予定です。</p>
       </Container>
     </section>
   )
