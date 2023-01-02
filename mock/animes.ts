cde8a811cdebc0a2c584c7843401926f705cce38
almightyk11 <takada.chopin.twitter@gmail.com>
2023-01-02 17:15:42 +0900

    feat: 見たアニメリスト作成(2022~2020)

---
+++ mock/animes.ts
@@ -0,0 +1,136 @@
+import { MockAnimeListByYear } from 'interfaces/anime'
+
+// api作成までの間直書きで対応
+export const mockAnimeListByYear: MockAnimeListByYear = [
+  {
+    animes: [
+      {
+        rating: 3,
+        title: 'THE FIRST SLAM DUNK',
+      },
+      {
+        rating: 1,
+        title: '機動戦士ガンダム 水星の魔女',
+      },
+      {
+        rating: 1,
+        title: 'チェンソーマン',
+      },
+      {
+        rating: 0,
+        title: '異世界おじさん',
+      },
+      {
+        rating: 0,
+        title: 'オーバーロードⅣ',
+      },
+      {
+        rating: 2,
+        title: 'リコリス・リコイル',
+      },
+      {
+        rating: 1,
+        title: 'かぐや様は告らせたい-ウルトラロマンティック-',
+      },
+      {
+        rating: 0,
+        title: 'SPY×FAMILY',
+      },
+      {
+        rating: 0,
+        title: 'トモダチゲーム',
+      },
+      {
+        rating: 2,
+        title: '進撃の巨人 The Final Season Part2',
+      },
+      {
+        rating: 1,
+        title: 'その着せ替え人形は恋をする',
+      },
+    ],
+    year: '2022年',
+  },
+  {
+    animes: [
+      {
+        rating: 2,
+        title: '86―エイティシックス― 第2クール',
+      },
+      {
+        rating: 1,
+        title: 'takt op.Destiny',
+      },
+      {
+        rating: 0,
+        title: '無職転生 ～異世界行ったら本気だす～ 第2クール',
+      },
+      {
+        rating: 2,
+        title: '86―エイティシックス―',
+      },
+      {
+        rating: 0,
+        title: 'シャドーハウス',
+      },
+      {
+        rating: 1,
+        title: '東京リベンジャーズ',
+      },
+      {
+        rating: 2,
+        title: 'Vivy - Fluorite Eyeʼs Song-',
+      },
+      {
+        rating: 1,
+        title: '美少年探偵団',
+      },
+      {
+        rating: 0,
+        title: '無職転生 ～異世界行ったら本気だす～',
+      },
+      {
+        rating: 1,
+        title: 'Re:ゼロから始める異世界生活 2nd season（後半クール）',
+      },
+    ],
+    year: '2021年',
+  },
+  {
+    animes: [
+      {
+        rating: 0,
+        title: 'アクダマドライブ',
+      },
+      {
+        rating: 2,
+        title: '進撃の巨人 The Final Season ',
+      },
+      {
+        rating: 1,
+        title: '呪術廻戦',
+      },
+      {
+        rating: 1,
+        title: '魔女の旅々',
+      },
+      {
+        rating: 1,
+        title: '無能なナナ',
+      },
+      {
+        rating: 1,
+        title: 'Re:ゼロから始める異世界生活 2nd season（前半クール）',
+      },
+      {
+        rating: 1,
+        title: 'かぐや様は告らせたい？～天才たちの恋愛頭脳戦～',
+      },
+      {
+        rating: 0,
+        title: 'ID:INVADED イド：インヴェイデッド',
+      },
+    ],
+    year: '2020年',
+  },
+]
