97264036aa5d3fd71c0c2941a467d4694962e54b
almightyk11 <takada.chopin.twitter@gmail.com>
2023-01-03 18:49:41 +0900

    refactor: headタグを一時的に_app.texに記述

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,36 @@ import { MantineProvider } from '@mantine/core'
 import { Navbar } from 'components/Navbar'
 import { TopSection } from 'components/TopSection'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Navbar />
-      <TopSection />
-      <Component {...pageProps} />
-    </MantineProvider>
+    <>
+      <Head>
+        <title>オールマイティ・kのサイト（仮）</title>
+        <meta
+          name='description'
+          content='自分のピアノ演奏動画、見たアニメリスト、弾いた・弾いてみたい曲リストetcなどを載せた簡易プロフィールサイトを新しく使う技術の勉強・練習も兼ねて作成します。'
+        />
+
+        {/* twitter card link */}
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:title' content='オールマイティ・Kのプロフィールサイト（作成中）' />
+        <meta
+          name='twitter:description'
+          content='自分のピアノ演奏動画、見たアニメリスト、弾いた・弾いてみたい曲リストetcなどを載せた簡易プロフィールサイトを新しく使う技術の勉強・練習も兼ねて作成します。'
+        />
+        <meta
+          name='twitter:image'
+          content='https://my-profile-site-khaki.vercel.app/twitter_card.jpg'
+        />
+      </Head>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <Navbar />
+        <TopSection />
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { Container } from '@mantine/core'
 import { MovieCardList } from 'components/Movie'
 import { mockMovies } from 'mock/movies'
-import Head from 'next/head'
 
 // SG（動画取得処理で止まってするので一旦保留）
 // export const getStaticProps: GetStaticProps<{ movies: Movie[] }> = async () => {
@@ -17,26 +16,6 @@ import Head from 'next/head'
 const Home = () => {
   return (
     <>
-      <Head>
-        <title>オールマイティ・kのサイト（仮）</title>
-        <meta
-          name='description'
-          content='自分のピアノ演奏動画、見たアニメリスト、弾いた・弾いてみたい曲リストetcなどを載せた簡易プロフィールサイトを新しく使う技術の勉強・練習も兼ねて作成します。'
-        />
-
-        {/* twitter card link */}
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta name='twitter:title' content='オールマイティ・Kのプロフィールサイト（作成中）' />
-        <meta
-          name='twitter:description'
-          content='自分のピアノ演奏動画、見たアニメリスト、弾いた・弾いてみたい曲リストetcなどを載せた簡易プロフィールサイトを新しく使う技術の勉強・練習も兼ねて作成します。'
-        />
-        <meta
-          name='twitter:image'
-          content='https://my-profile-site-khaki.vercel.app/twitter_card.jpg'
-        />
-      </Head>
-
       {/* 演奏動画一覧 */}
       <section className='min-h-screen py-3 bg-slate-200'>
         <Container>
